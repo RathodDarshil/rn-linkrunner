da81e854f31bc70915a59d76f752a7c48507f46f
Darshil Rathod <darshilrathod0@gmail.com>
2024-04-25 20:40:27 +0530

    fix: version increment

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import DeviceInfo, {
 } from 'react-native-device-info';
 import EncryptedStorage from 'react-native-encrypted-storage';
 
-const package_version = '0.2.2';
+const package_version = '0.3.0';
 const app_version = DeviceInfo.getVersion();
 const EncryptedStorageTokenName = 'linkrunner-token';
 
